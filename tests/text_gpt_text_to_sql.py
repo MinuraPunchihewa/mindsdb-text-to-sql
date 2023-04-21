72b78e1bb8f61841ff9b2ebdd359b1c06438f0bf
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-21 23:10:39 +0530

    added the main unit test module for the package and included simple test to test SELECTs

---
+++ tests/text_gpt_text_to_sql.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+from dotenv import load_dotenv
+
+from mindsdb_text_to_sql import GPTTextToSQL
+
+load_dotenv()
+
+
+class TestGPTTextToSQL(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.tts = GPTTextToSQL(os.environ.get("API_KEY"))
+
+    def test_select_query(self):
+        self.assertEqual(
+            self.tts.convert_text_to_sql("Get the top 10 rows from the customer table of the databricks_datasource who earn a salary greater than 100000"),
+            "SELECT * FROM databricks_datasource.customer\nWHEREsalary > 100000;",
+        )
